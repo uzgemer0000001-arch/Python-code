80056c9517b0ed8e14737cc3a9d1dab879b048bc
uzgemer0000001-arch <uzgemer0000001@gmail.com>
2026-05-21 15:15:51 +0300

    Add files via upload

---
+++ 4_dars.py
@@ -0,0 +1,31 @@
+# input 
+
+
+# a = str(input("matn: "))
+
+
+
+
+# b = int(input("Son: "))
+# print(b)
+
+
+
+
+
+# c = float(input("Son yoz: "))
+# print(c)
+
+
+# ism = str(input("Ism: "))
+# ish = str(input("Ish "))
+# now_year = int(input("Hozirgi yil: "))
+# year = int(input("Tug'ilgan yili: "))
+# print("Mijozni ismi", ism)
+
+son = int(input("Qo'shiluvchi son kiriting: "))
+son_2 = int(input("Ayriluvchi son kiriting: "))
+natija = son_2 + son
+natija_2 = son - son_2
+print("Yig'indi",natija)
+print("Ayirma",natija_2)
