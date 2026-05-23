8b8e61177c09a08873ad5b8ff159adb89157fdcb
uzgemer0000001-arch <uzgemer0000001@gmail.com>
2026-05-23 16:23:54 +0500

    6-darsim
    bu boshlanishi

---
+++ 6_dars.py
@@ -0,0 +1,15 @@
+ism = str(input("Ismingizni kiriting: "))
+ism = ism.capitalize()
+yosh = int(input("Siz necha yoshsiz? "))
+if yosh >= 18:
+    print("Yaxshi siz voyaga yetgansiz")
+else:
+    print("Siz hali yoshsiz! ")
+
+print(yosh,"yoshli",ism)
+
+
+
+
+
+
